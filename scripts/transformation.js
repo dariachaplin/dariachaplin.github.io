2ee34b380b5ee8cf30733938d2669a87597c5b60
Daria Chaplin <dxc4643@g.rit.edu>
2022-03-27 16:38:35 -0400

    Beginning work on transformation game Basic HTML setup Filled out resources tab Created design for house

--- scripts/transformation.js
+++ scripts/transformation.js
@@ -1,5 +1,30 @@
 function setup() {
     createCanvas(400, 400);
-    background('blue');
+    background(210, 210, 210);
+
+    // Set up axis (with origin at 200, 200)
+    line(200, 10, 200, 390);
+    line(10, 200, 390, 200);
+
+    // Create shapes to form a house
+    noStroke();
+    angleMode(DEGREES);
+    translate(200, 200);
+    fill('blue');
+    square(-25, -25, 50);
+    fill('limegreen');
+    rect(0, 0, 15, 25);
+    fill('red');
+    triangle(-25, -25, 25, -25, 0, -50);
+
+    // Target house (placeholder)
+    translate(75, 75);
+    fill('dimgrey');
+    square(-25, -25, 50);
+    fill('darkgrey');
+    rect(0, 0, 15, 25);
+    fill('grey');
+    triangle(-25, -25, 25, -25, 0, -50);
+
     noLoop();
 }
