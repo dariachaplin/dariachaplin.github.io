12351c6bf820c960daea52460fc9538a29984597
Daria Chaplin <dxc4643@g.rit.edu>
2022-04-11 15:15:14 -0400

    Transformation game work Fixed the way solutions are checked - track transformed positions

--- scripts/transformation.js
+++ scripts/transformation.js
@@ -1,16 +1,54 @@
 let sliders = [];
+let coordThresh = 2, angleThresh = 5, scaleThresh = 0.2;
+
+// Position class used to keep track of transformed coordinates
+// Based on this solution:
+// https://github.com/ChristerNilsson/Transformer/blob/master/js/transformer.js
+class Position {
+    constructor(x, y, angle, scaleX, scaleY) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.scaleX = scaleX;
+        this.scaleY = scaleY;
+    }
+
+    translatePos(x, y) {
+        this.x += this.scaleX * x * cos(this.angle) - this.scaleY * y * sin(this.angle);
+        this.y += this.scaleY * y * cos(this.angle) + this.scaleX * x * sin(this.angle);
+    }
+
+    rotatePos(angle) {
+        this.angle = (this.angle + angle) % 360;
+    }
+
+    scalePos(x, y) {
+        this.scaleX *= x;
+        this.scaleY *= y;
+    }
+
+    approxEqualTo(otherPos) {
+        let coordsMatch = abs(this.x - otherPos.x) < coordThresh
+            && abs(this.y - otherPos.y) < coordThresh;
+        let anglesMatch = abs(this.angle - otherPos.angle) < angleThresh
+            || 360 - abs(this.angle - otherPos.angle) < angleThresh;
+        let scalesMatch = abs(this.scaleX - otherPos.scaleX) < scaleThresh
+            && abs(this.scaleY - otherPos.scaleY) < scaleThresh;
+        return (coordsMatch && anglesMatch && scalesMatch);
+    }
+}
 
 function setup() {
-    angleMode(DEGREES);
-    setUpAxis();
-
     // Display the 'Instructions' tab by default
     document.getElementById('Instructions').style.display = "block";
 
+    angleMode(DEGREES);
+    setUpAxis();
+
     // Placeholder-ish code to simulate a simple level
 
-    let target = [["T", 75, 0], ["T", 0, 75]];
-    let house = [["T", 0, 0], ["T", 0, 0]]
+    let target = [["T", 75, 0], ["T", 0, 75], ["S", 2, -0.5], ["R", 30]];
+    let house = [["T", 0, 0], ["T", 0, 0], ["S", 1, 1], ["R", 0]];
     transformHouse(house, target);
 
     s1 = createSlider(-400, 400);
@@ -26,6 +64,27 @@ function setup() {
         transformHouse(house, target);
     });
     sliders.push(s2);
+
+    s3 = createSlider(-3, 3, 0, 0.5);
+    s3.input(() => {
+        house[2][1] = s3.value();
+        transformHouse(house, target);
+    });
+    sliders.push(s3);
+
+    s4 = createSlider(-3, 3, 0, 0.5);
+    s4.input(() => {
+        house[2][2] = s4.value();
+        transformHouse(house, target);
+    });
+    sliders.push(s4);
+
+    s5 = createSlider(0, 360);
+    s5.input(() => {
+        house[3][1] = s5.value();
+        transformHouse(house, target);
+    });
+    sliders.push(s5);
 
     noLoop();
 }
@@ -62,34 +121,12 @@ function star() {
     pop();
 }
 
-// Compare the user's transforms with target transforms
-// Return true only if they are (approximately) the same
-function sameTransforms(house, target) {
-    if(house.length != target.length) { return false; }
-
-    let cur1, cur2;
-    let thresh = 5;
-
-    for( let i = 0; i < house.length; i++) {
-        cur1 = house[i];
-        cur2 = target[i];
-
-        // Compare transformation type
-        if(cur1[0] != cur2[0]) { return false; }
-
-        // Compare first parameter
-        if(abs(cur1[1] - cur2[1]) > thresh) { return false; }
-
-        // Compare second parameter, if there is one
-        if(cur1[0] != "R" && abs(cur1[2] - cur2[2]) > thresh) { return false; }
-    }
-
-    return true;
-}
-
 // Perform the given list of transforms to prepare for drawing
+// Return the transformed position
 function performTransforms(lst) {
-    if(lst.length == 0) { return; }
+    let pos = new Position(0, 0, 0, 1, 1);
+
+    if(lst.length == 0) { return pos; }
 
     let cur;
     for(let i = 0; i < lst.length; i++) {
@@ -97,16 +134,21 @@ function performTransforms(lst) {
         switch(cur[0]) {
             case "T":
                 translate(cur[1], cur[2]);
+                pos.translatePos(cur[1], cur[2]);
                 break;
             case "R":
-                // TODO Check whether angle needs to be negated
-                rotate(cur[1]);
+                // Negate angle (p5.js does positive rotations clockwise)
+                rotate(-cur[1]);
+                pos.rotatePos(-cur[1]);
                 break;
             case "S":
                 scale(cur[1], cur[2]);
+                pos.scalePos(cur[1], cur[2]);
                 break;
         }
     }
+
+    return pos;
 }
 
 // Redraw both houses to reflect updates to transforms
@@ -117,31 +159,32 @@ function transformHouse(house, target) {
     translate(200, 200);
 
     // Redraw current target house
-    performTransforms(target);
+    push();
+    let targetPos = performTransforms(target);
     fill('dimgrey');
     square(-25, -25, 50);
     fill('darkgrey');
     rect(0, 0, 15, 25);
     fill('grey');
     triangle(-25, -25, 25, -25, 0, -50);
-
-    // Reset origin to 200, 200 again
-    resetMatrix();
-    translate(200, 200);
+    pop();
 
     // Apply current transformations and redraw main house
-    performTransforms(house);
+    push();
+    let mainPos = performTransforms(house);
+    console.log(mainPos); // TODO remove after debugging complete
     fill('blue');
     square(-25, -25, 50);
     fill('limegreen');
     rect(0, 0, 15, 25);
     fill('red');
     triangle(-25, -25, 25, -25, 0, -50);
+    pop();
 
     // Draw a star if the transformed house lines up with the target
-    if(sameTransforms(house, target)) {
+    if(mainPos.approxEqualTo(targetPos)) {
         star();
-        sliders[0].remove();
-        sliders[1].remove();
+        sliders.forEach(slider => slider.remove());
+        sliders = [];
     }
 }
