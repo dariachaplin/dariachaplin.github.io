d7795689ff6364ce834f2c86563f7a15dbeb154b
Daria Chaplin <dxc4643@g.rit.edu>
2022-04-13 14:16:03 -0400

    Transformation game work Set up levels and navigation between them Started setting up transformation inputs

--- scripts/transformation.js
+++ scripts/transformation.js
@@ -1,3 +1,4 @@
+let level = 1, levelLabel = 'Level 1', maxLevel = 5;
 let sliders = [];
 let coordThresh = 2, angleThresh = 5, scaleThresh = 0.2;
 
@@ -45,49 +46,170 @@ function setup() {
     angleMode(DEGREES);
     setUpAxis();
 
-    // Placeholder-ish code to simulate a simple level
+    // Set up level navigation buttons and a label to display the current level
 
-    let target = [["T", 75, 0], ["T", 0, 75], ["S", 2, -0.5], ["R", 30]];
-    let house = [["T", 0, 0], ["T", 0, 0], ["S", 1, 1], ["R", 0]];
+    let pLevel = createP(levelLabel);
+    pLevel.position(185, 470);
+    pLevel.id('level-label');
+
+    let bPrevious = createButton('Previous');
+    bPrevious.position(8, 486);
+    bPrevious.mousePressed(() => {
+        if(level == maxLevel) { bNext.show(); }
+        level -= 1;
+        document.getElementById('level-label').innerHTML = 'Level ' + level;
+        if(level == 1) { bPrevious.hide(); }
+        changeLevel();
+    });
+    bPrevious.hide();
+
+    let bNext = createButton('Next');
+    bNext.position(365, 486);
+    bNext.mousePressed(() => {
+        if(level == 1) { bPrevious.show(); }
+        level += 1;
+        document.getElementById('level-label').innerHTML = 'Level ' + level;
+        if(level == maxLevel) { bNext.hide(); }
+        changeLevel();
+    });
+
+    // Start the user off at level 1
+    level1();
+
+    document.getElementById('val-1').setAttribute('min', 1);
+
+    noLoop();
+}
+
+// Functions to add to and update the sequence of transforms on 'Instructions' tab
+
+function setupInput(num, min, max, label) {
+    let val = document.getElementById('val-' + num);
+    val.style.visibility = 'visible';
+    val.setAttribute('min', min);
+    val.setAttribute('max', max);
+    val.value = "";
+    
+    let valLabel = document.getElementById('val-' + num + '-label');
+    valLabel.style.visibility = 'visible';
+    valLabel.innerHTML = label + ':';
+}
+
+function hideInput(num) {
+    let val = document.getElementById('val-' + num);
+    val.style.visibility = 'hidden';
+    val.value = "";
+
+    let valLabel = document.getElementById('val-' + num + '-label');
+    valLabel.style.visibility = 'hidden';
+}
+
+function addTranslation() {
+    // TODO clamp translation values to fit to canvas based on current position
+    setupInput('1', -200, 200, 'X');
+    setupInput('2', -200, 200, 'Y');
+}
+
+function addRotation() {
+    setupInput('1', -360, 360, 'Angle (degrees)');
+    hideInput('2');
+}
+
+function addScaling() {
+    setupInput('1', -3, 3, 'X');
+    setupInput('2', -3, 3, 'Y');
+}
+
+function updateVal1() {
+    let val = parseFloat(document.getElementById('val-1').value);
+    // TODO
+}
+
+function updateVal2() {
+    let val = parseFloat(document.getElementById('val-2').value);
+    // TODO
+}
+
+function updateTransforms(transforms) {
+    let str = "";
+    transforms.forEach(transform => {
+        str += transform[0] + "(" + transform[1];
+        if(transform.length == 3) { str += ", " + transform[2]; }
+        str += ") * ";
+    });
+
+    let sequence = document.getElementById('sequence');
+    if(str == "") { sequence.innerHTML = "None!"; }
+    else { sequence.innerHTML = str + "HOUSE"; }
+}
+
+// Level changing and specific level implementation functions
+
+function changeLevel() {
+    // Clear out previous level's sliders
+    sliders.forEach(slider => slider.remove());
+    sliders = [];
+
+    // Clear out input boxes
+    hideInput('1');
+    hideInput('2');
+
+    if(level == 1) { level1(); }
+    else if(level == 2) { level2(); }
+    else if(level == 3) { level3(); }
+    else if(level == 4) { level4(); }
+    else if(level == 5) { level5(); }
+}
+
+function level1() {
+    let target = [["T", -45, -100]];
+    let house = [["T", 0, 0], ["T", 0, 0]];
     transformHouse(house, target);
 
-    s1 = createSlider(-400, 400);
+    s1 = createSlider(-200, 200);
+    s1.value(0);
+    s1.position(9, 510);
     s1.input(() => {
         house[0][1] = s1.value();
         transformHouse(house, target);
     });
     sliders.push(s1);
 
-    s2 = createSlider(-400, 400);
+    s2 = createSlider(-200, 200);
+    s2.value(0);
+    s2.position(150, 510);
     s2.input(() => {
         house[1][2] = s2.value();
         transformHouse(house, target);
     });
     sliders.push(s2);
-
-    s3 = createSlider(-3, 3, 0, 0.5);
-    s3.input(() => {
-        house[2][1] = s3.value();
-        transformHouse(house, target);
-    });
-    sliders.push(s3);
-
-    s4 = createSlider(-3, 3, 0, 0.5);
-    s4.input(() => {
-        house[2][2] = s4.value();
-        transformHouse(house, target);
-    });
-    sliders.push(s4);
-
-    s5 = createSlider(0, 360);
-    s5.input(() => {
-        house[3][1] = s5.value();
-        transformHouse(house, target);
-    });
-    sliders.push(s5);
-
-    noLoop();
 }
+
+function level2() {
+    let target = [["T", 100, 100], ["R", 90]];
+    let house = [];
+    transformHouse(house, target);
+}
+
+function level3() {
+    let target = [["T", -100, 0], ["S", 2, 2]];
+    let house = [];
+    transformHouse(house, target);
+}
+
+function level4() {
+    let target = [["S", 1.5, 1], ["R", 180], ["T", -45, 50]];
+    let house = [];
+    transformHouse(house, target);
+}
+
+function level5() {
+    let target = [["R", 30], ["S", 3, 1], ["T", 30, 0], ["R", -60]];
+    let house = [];
+    transformHouse(house, target);
+}
+
+// Functions to perform transformations and check for solution matches
 
 // Set up gray canvas and axis (with origin at 200, 200)
 function setUpAxis() {
@@ -133,8 +255,9 @@ function performTransforms(lst) {
         cur = lst[i];
         switch(cur[0]) {
             case "T":
-                translate(cur[1], cur[2]);
-                pos.translatePos(cur[1], cur[2]);
+                // Negate y-translations (p5.js canvas origin is top left)
+                translate(cur[1], -cur[2]);
+                pos.translatePos(cur[1], -cur[2]);
                 break;
             case "R":
                 // Negate angle (p5.js does positive rotations clockwise)
@@ -172,6 +295,7 @@ function transformHouse(house, target) {
     // Apply current transformations and redraw main house
     push();
     let mainPos = performTransforms(house);
+    updateTransforms(house);
     console.log(mainPos); // TODO remove after debugging complete
     fill('blue');
     square(-25, -25, 50);
